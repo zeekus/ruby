9fcc5b3c5807f218b86a6ac43890a7ccf0dc7f18
Zeekus <ipmasqman@gmail.com>
2020-12-02 20:40:28 -0500

    something changed

--- ruby_time_components.rb
+++ ruby_time_components.rb
@@ -6,12 +6,25 @@ require 'time'
 
 time=Time.now
 
+  #####################
+  #human readable times
+  #####################
+  day_map={ "sun" => 0, 
+          "mon" => 1, 
+          "tue" => 2, 
+          "wed" => 3 ,
+          "thu" => 4, 
+          "fri" => 5, 
+          "sat" => 6
+  }
+
+
 # Components of a Time
 puts "Current Time : #{time}"
 puts "year       :#{time.year}"    # => Year of the date 
 puts "month      :#{time.month}"   # => Month of the date (1 to 12)
 puts "day        :#{time.day}"     # => Day of the date (1 to 31 )
-puts "wday       :#{time.wday}"    # => 0: Day of week: 0 is Sunday
+puts "wday       :#{time.wday}}"    # => 0: Day of week: 0 is Sunday
 puts "yday       :#{time.yday}"    # => 365: Day of year
 puts "hour       :#{time.hour}"    # => 23: 24-hour clock
 puts "min        :#{time.min}"     # => 59
--- test_hash.rb
+++ test_hash.rb
@@ -1,12 +1,15 @@
+#!/usr/bin/ruby
+#lang: ruby
+#filename: hash_testing.rb
+#description: playing with hashes
 require 'time'
 holidays= Hash.new( "holidays" )
 holidays= {"12-31-2020" => "New Years", "01-14-2021" => "MLK"}
 holidays.store("12-25-2020",  "Christmas")
 
-holidays.each { |elem| 
-   puts "#{elem[0]}, #{elem[1]}"
-}
-
+#holidays.each { |elem| 
+#   puts "#{elem[0]}, #{elem[1]}"
+#}
 
 holidays=holidays.sort
 
