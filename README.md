06a319fd305a3c9ae9dc6c702eec79f54e6fa0c0
Ted.Knab <ted.knab@ssh1p.chesapeakebay.net>
2020-12-10 14:33:36 -0500

    updated timesheet helper

--- README.md
+++ README.md
@@ -4,19 +4,28 @@ It showcases some of things I can do.
 
 Filename | Description | Language 
 ----------- | ----------- | ---------- 
-top_10_hits.rb | garbage code needs re-work gets Top 10 requests by IPAddress per hour. | ruby
-
-find_target_day_in_month.rb | target a specific day |  ruby
+argv.rb | parsing data from argv with ruby  |  ruby
 
 canned_response_generator | provide canned responses to customers when they ask about reports. | ruby
 
-list_federal_holidays.rb | generate list of holiday work days for 13 years |  ruby
-
-argv.rb | parsing data from argv with ruby  |  ruby
-
-ruby_time_components.rb | maps out time elements ruby can see by default |  ruby
+find_target_day_in_month.rb | target a specific day |  ruby
 
 generate_README_md.rb | tool to automate the Generation of README.md files. |  ruby
 
+list_federal_holidays.rb | generate list of holiday work days for 100 years |  ruby
+
+list_federal_holidaysv1.rb | generate list of holiday work days for 100 years |  ruby
+
+list_federal_holidaysv2.rb | generate list of holiday work days for 100 years |  ruby
+
 list_potential_workdays_for_2020.rb | generate list of potential work days for 2020 |  ruby
 
+ruby_time_components.rb | maps out time elements ruby can see by default |  ruby
+
+test_hash.rb | playing with hashes |  ruby
+
+top_10_hits.rb | garbage code needs re-work gets Top 10 requests by IPAddress per hour. | ruby
+
+calculate_hour_needed_to_fullfil_timesheet.rb | calc how many hours are needed to fufil the time card |  ruby
+
+output_from_calculate_hours_needed_to_work.txt | unknown | unknown
---
+++ calculate_hour_needed_to_fullfil_timesheet.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/ruby
+#language: Ruby
+#filename: calculate_hour_needed_to_fullfil_timesheet.rb
+#description: calc how many hours are needed to fufill the time period
+
+start_of_pay_period="12/01/2020"  #start
+end_of_pay_period="12/15/2020"  #end
+
+require 'time'
+
+my_start=Time.strptime(start_of_pay_period,"%m/%d/%y")
+my_end=Time.strptime(end_of_pay_period,"%m/%d/%y")
+
+one_day_adv=(60 * 60 * 24) #one day in seconds
+
+hours_completed=0
+puts "length of ARGV array is: " + ARGV.length.to_s
+
+#####################
+#checking arguments
+######################
+if ARGV.length > 0 
+   hours_completed=ARGV[0].to_i
+   puts "hours completed entered with #{hours_completed}"
+else
+   puts "no hours completed"
+   puts "hours completed entered with #{hours_completed}"
+   
+end
+
+
+nextday=my_start
+count=0
+
+while true
+  formatted=nextday.strftime("%m/%d/%Y")
+  puts "#{formatted} is Mon"  if nextday.monday? 
+  puts "#{formatted} is Tue"  if nextday.tuesday? 
+  puts "#{formatted} is Wed"  if nextday.wednesday? 
+  puts "#{formatted} is Thu" if nextday.thursday? 
+  puts "#{formatted} is Fri" if nextday.friday? 
+  count=count+1 if nextday.saturday? != true and nextday.sunday? != true
+  nextday=nextday+one_day_adv
+
+  if nextday >  my_end
+    break
+  end
+
+end
+
+puts "use: calculated_hours_needed_to_work.rb 20 #for 20 hours worked" 
+
+puts "day count is #{count}"
+hours_needed=count*8
+puts "total hours needed for the period is #{hours_needed}"
+puts "total hours completed is #{hours_completed}"
+hours_remaining=hours_needed
+hours_remaining=hours_remaining - hours_completed
+puts "total hours remaining is #{hours_remaining}"
+
