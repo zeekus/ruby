b03d4679b16b078fadfbd48660fdda257d2592c1
Theodore Knab <ipmasqman@gmail.com>
2021-09-30 15:27:03 -0400

    edit

--- jruby/find_mouse_location.rb
+++ jruby/find_mouse_location.rb
@@ -8,14 +8,28 @@ require 'java'
 #gets mouse location class
 
 
-java_import 'java.awt.event.InputEvent' #moves mouse and typing class
-java_import 'java.awt.MouseInfo'        #gets mouse location class
-java_import 'java.awt.Color'            #gets pixel color at mouse  location
-java_import 'java.awt.event.KeyEvent'    #keyboard input class
+java_import 'java.awt.event.InputEvent'  #typing class and 
+java_import 'java.awt.MouseInfo'         #gets mouse location class
+#java_import 'java.awt.event.MouseEvent'  #mouse clicks
+java_import 'java.awt.Color'             #gets pixel color at mouse  location
+#java_import 'java.awt.event.KeyEvent'    #keyboard input class
+#java_import 'java.awt.event.MouseMotionListener' 
+
+
 
 #ref http://drpeterjones.com/colorcalc/ for color codes
 
+# def press_mouse_button(robot)
+#   robot.mousePress(InputEvent.BUTTON1)
+#   robot.mouseRelease(InputEvent.BUTTON1)
+#  puts "mouse click"
+#      sleep(0.1)
+
+#    return robot
+# end
+
 def get_current_mouse_location(robot)
+    #robot=press_mouse_button(robot)
     cur=[]
     #my_x=java.awt.MouseInfo.getPointerInfo().getLocation().x
     my_x=MouseInfo.getPointerInfo().getLocation().x
