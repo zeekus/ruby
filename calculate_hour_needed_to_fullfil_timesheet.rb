bcf3c461e313a4216642cb65e1dcf96ce7283095
zeekus <ipmasqman@gmail.com>
2021-10-13 16:12:21 -0400

    edit

--- calculate_hour_needed_to_fullfil_timesheet.rb
+++ calculate_hour_needed_to_fullfil_timesheet.rb
@@ -2,15 +2,75 @@
 #language: Ruby
 #filename: calculate_hour_needed_to_fullfil_timesheet.rb
 #description: calc how many hours are needed to fufill the time period
-
-start_of_pay_period="12/16/2020"  #start
-end_of_pay_period="12/31/2020"  #end
-
 require 'time'
 
-my_start=Time.strptime(start_of_pay_period,"%m/%d/%y")
-my_end=Time.strptime(end_of_pay_period,"%m/%d/%y")
+def get_todays_number(now)
+ 
+  if now.sunday?
+    return 0
+  elsif now.monday?
+    return 1
+  elsif now.tuesday?
+    return 2
+  elsif now.wednesday?
+    return 3
+  elsif now.thursday?
+    return 4
+  elsif now.friday?
+    return 5
+  elsif now.saturday?
+    return 6
+  end
+end
+
+def get_monday(day_value)
+    
+    if day_value == 1    #monday
+       puts monday
+       return Time.now
+    elsif day_value < 1  #sunday
+       puts sunday
+       return Time.now() + (60*60*24) 
+    elsif day_value > 1
+      return Time.now() - ((day_value-1) * (60*60*24))
+    else
+      exit 1 
+    end
+end
+
+def get_friday(day_value)
+    if day_value == 5  #friday
+       return Time.now
+    elsif day_value < 5  
+      return Time.now() + ( (5-day_value) * (60*60*24))
+    else
+      return Time.now() - ((day_value-5) * (60*60*24))
+    end
+end
+
+
 now=Time.now
+get_week=Time.now().strftime "%W"
+todays_num=get_todays_number(now)
+
+puts "now is #{now}"
+puts "week is #{get_week}"
+puts "today's number is #{todays_num}" 
+
+start_of_pay_period=get_monday(todays_num).strftime('%m/%d/%Y')
+end_of_pay_period=get_friday(todays_num).strftime('%m/%d/%Y')
+
+puts "start #{start_of_pay_period}"
+puts "end #{end_of_pay_period}"
+
+
+#start_of_pay_period="10/11/2021"  #start
+#end_of_pay_period="10/15/2021"  #end
+
+
+
+my_start=Time.strptime(start_of_pay_period,"%m/%d/%Y")
+my_end=Time.strptime(end_of_pay_period,"%m/%d/%Y")
 mytoday=now.strftime("%m/%d/%Y")
 
 one_day_adv=(60 * 60 * 24) #one day in seconds
