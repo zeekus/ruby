b651a4e4e9a7cdea509520b432980516bbb0d4a7
zeekus <ipmasqman@gmail.com>
2021-12-02 16:58:59 -0500

    edit

---
+++ cash_out_senario.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+#filename: cash_out_senario.rb
+#description: calculate tax and reinvest senarios. 
+#
+
+speculation_price_of_BTC=[ 165000, 201000, 349000, 450000 ]
+sell_coins=STDIN.gets.chomp()
+
+
+#credits: Rick Moore 
+#source https://dirklo.medium.com/formatting-number-strings-in-ruby-4da35d5282e3
+def format_number(number)
+    whole, decimal = number.to_s.split(".")
+    num_groups = whole.chars.to_a.reverse.each_slice(3)
+    whole_with_commas = num_groups.map(&:join).join(',').reverse
+    [whole_with_commas, decimal].compact.join(".")
+end
+
+
+for x in speculation_price_of_BTC
+  #format as decimal float
+  puts "--------------------"
+  after_tax  = ( x.to_f * sell_coins.to_f * 0.50 )
+  buy_back=( x.to_f * 0.3)
+  puts "1) BTC price " + format_number('%.2f' % x)
+  expected_retrace =  after_tax.to_f / buy_back.to_f
+  puts "2) after tax for sell of #{sell_coins.to_i} BTC:" + format_number('%.2f' % after_tax)
+  puts "3) " + format_number('%.4f' % expected_retrace) + " bitcoins @ #{buy_back}"
+end
+
+
