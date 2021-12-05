d97a9ddeab72436ddf17a18c907e13c843daaf07
zeekus <ipmasqman@gmail.com>
2021-12-04 22:17:56 -0500

    deconstructing functions

--- jruby/find_mouse_location.rb
+++ jruby/find_mouse_location.rb
@@ -1,51 +1,23 @@
 #!/usr/bin/jruby
 #filename: move_mouse.rb
+#description: gets the current mouse location
 
 require 'java'
-#system("export DISPLAY=localhost:0.0") #windows
-
-
-#gets mouse location class
-
 
 java_import 'java.awt.event.InputEvent'  #typing class and 
 java_import 'java.awt.MouseInfo'         #gets mouse location class
-#java_import 'java.awt.event.MouseEvent'  #mouse clicks
 java_import 'java.awt.Color'             #gets pixel color at mouse  location
-#java_import 'java.awt.event.KeyEvent'    #keyboard input class
-#java_import 'java.awt.event.MouseMotionListener' 
 
 
-
-#ref http://drpeterjones.com/colorcalc/ for color codes
-
-# def press_mouse_button(robot)
-#   robot.mousePress(InputEvent.BUTTON1)
-#   robot.mouseRelease(InputEvent.BUTTON1)
-#  puts "mouse click"
-#      sleep(0.1)
-
-#    return robot
-# end
-
-def get_current_mouse_location(robot)
-    #robot=press_mouse_button(robot)
-    cur=[]
-    #my_x=java.awt.MouseInfo.getPointerInfo().getLocation().x
+def get_mouse_loc(robot)
     my_x=MouseInfo.getPointerInfo().getLocation().x
-    #my_y=java.awt.MouseInfo.getPointerInfo().getLocation().y
     my_y=MouseInfo.getPointerInfo().getLocation().y
-
-    #get current mouse
-    puts my_x
-    puts my_y
-    cur=[my_x,my_y]
-    return cur
+    puts "function - get_mouse_location [#{my_x},#{my_y}]"
+    return my_x,my_y
 end
 
 puts "loading robot super class"
 robot= java.awt.robot.new
 
-puts "getting loc"
-location=get_current_mouse_location(robot)
-puts location.to_s
+x,y=get_mouse_loc(robot)
+puts "returned value - current mouse location is [#{x},#{y}]"
