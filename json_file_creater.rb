862e07524a0b89f468fec639c8baeaf1d1f986e2
zeekus <ipmasqman@gmail.com>
2021-12-06 10:14:13 -0500

    edit

---
+++ json_file_creater.rb
@@ -0,0 +1,26 @@
+require 'json'
+
+#read json file in
+
+my_json_file=("/var/tmp/my_first_json_file.json")
+if File.exist?(my_json_file)
+  puts "file exits. opening file..."
+  file = File.read(my_json_file)
+  data_hash = JSON.parse(file)
+  for key,value in data_hash
+     puts "#{key}:#{value}"
+  end
+
+  #data_hash = JSON.load(file) #raw data
+else
+  puts "json file is missing at #{my_json_file}. We are creating one."
+  tmp_Hash={
+        "location1" => "[125,577]",
+        "location2" => "[0,0]" }
+  File.open(my_json_file,'w') do |f| 
+    f.write(tmp_Hash.to_json)
+  end
+end
+
+
+
