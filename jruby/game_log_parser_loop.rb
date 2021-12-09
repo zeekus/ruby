39f454ee69f9c988e3652660e81dc4cdd23d627b
zeekus <ipmasqman@gmail.com>
2021-12-09 09:25:24 -0500

    cleaned up log reader

--- jruby/game_log_parser_loop.rb
+++ jruby/game_log_parser_loop.rb
@@ -3,9 +3,7 @@
 #description: function looks for active log file and then reads the messages. We only pull the relevant new data.
 require 'time'
 
-
-
-def is_log_entry_current(loginfo,counter)
+def is_log_entry_current(loginfo)
   debug=0
 
   #string stripping 
@@ -17,7 +15,6 @@ def is_log_entry_current(loginfo,counter)
   logtime=DateTime.strptime(logtime, '%Y.%m.%d %H:%M:%S') #convert logtime sting to usable variable
   logtime_secs=logtime.strftime("%s")                     #convert time to seconds
   puts "debug log time- logtime_secs #{logtime_secs}" if debug==1
-
 
   current_secs=Time.now.utc.strftime("%s")                #our logs get current time in UTC seconds
   puts "debug - current_secs #{current_secs}" if debug==1
@@ -36,40 +33,60 @@ def log_reader()
  #####################
  ##bash equivalent
  ##file=system("find /home/$USER/Documents/EVE/logs/Gamelogs -cmin -1 -exec ls -lah {} ';'")
- logfile_loc_glob="/home/zteddy/Documents/EVE/logs/Gamelogs/*.txt" #glob for all
+ my_homedir=Dir.home
+ logfile_loc_glob="#{my_homedir}/Documents/EVE/logs/Gamelogs/*.txt" #glob for all
+
  myfile=Dir.glob(logfile_loc_glob).max_by { |file_name| File.ctime(file_name) } 
-
- filesize=0  #get size of the file
- file=File.open(myfile,"r")
- filesize=file.readlines.size
- puts "'debug filesize has #{filesize}' lines"
-
- #only run if file size is greater than 5
- if filesize < 5
-  puts "exiting. Listener is active but file is too small. Try exiting the station."
-  exit
+ if File.exists?(myfile)
+   puts "debug my last log is #{myfile}"
+   file=File.open(myfile) #read file
+   file_data=file.readlines.map(&:chomp) #attemping to get file data without new lines
+   file.close #closing file
+   filesize=0  #get size of the file
+   filesize=file_data.size
+   puts "'debug gamelog file has #{filesize}' lines"
+   #only run if file size is greater than 5
+   if filesize < 5
+    puts "exiting. Listener is active but file is too small. Try exiting the station."
+    exit
+   end
+ else 
+   puts "missing file #{myfile} exiting"
+   exit
  end
+ 
+#  puts "is file_data an array ?"
+#  p file_data.instance_of? Array
 
- #get last 3 lines of the log
- last_3=IO.readlines(myfile)[-3..-1]
- last_3.each do |line|
+ last_3=file_data[-3..-1]  #get last 3 lines of the file_data
+#  puts "is last_3 an array ?"
+#  p last_3.instance_of? Array
 
+ last_3.each do |line|  #look at it
    if /^\[/.match(line) #sometimes the lines don't have the time ignore them
-     result=is_log_entry_current(line.chomp,counter) #current log entry only
+     result=is_log_entry_current(line.chomp) #current log entry only
      if result ==1
         string = line.split("(None) ")[1]#remove first part of line so just get the jumping info
+        # puts "is string an array ?"
+        # p string.instance_of? Array
         return string
      end
    end
   end
+  return "" #return an empty string to prevent an object pointer from getting returned and messsing up things
 end
 
-jump_complete=0
-until jump_complete==1
-  sleep 1
-  parsed_log=log_reader()
-  if parsed_log =~ /jumping/i 
-    puts parsed_log
-    jump_complete =1
-  end
-end
+  
+  #start of prototype loop
+  jump_complete=0
+  until jump_complete==1
+    sleep 1
+    parsed_log=log_reader()
+    # puts "is parsed_log an array ?"
+    # p parsed_log.instance_of? Array
+
+    if parsed_log =~ /jumping/i 
+      puts "'#{parsed_log}'"
+      jump_complete =1
+    end
+  end # end of prototype loop
--- jruby/json_auto_jump_loop.rb
+++ jruby/json_auto_jump_loop.rb
@@ -360,45 +360,47 @@ def log_reader()
   #####################
   ##bash equivalent
   ##file=system("find /home/$USER/Documents/EVE/logs/Gamelogs -cmin -1 -exec ls -lah {} ';'")
-  logfile_loc_glob="/home/zteddy/Documents/EVE/logs/Gamelogs/*.txt" #glob for all
+  my_homedir=Dir.home
+  logfile_loc_glob="#{my_homedir}/Documents/EVE/logs/Gamelogs/*.txt" #glob for all
+ 
   myfile=Dir.glob(logfile_loc_glob).max_by { |file_name| File.ctime(file_name) } 
- 
-  filesize=0  #get size of the file
-  file=File.open(myfile,"r")
-  filesize=file.readlines.size
-  puts "'debug filesize has #{filesize}' lines"
- 
-  #only run if file size is greater than 5
-  if filesize < 5
-   puts "exiting. Listener is active but file is too small. Try exiting the station."
-   exit
+  if File.exists?(myfile)
+    puts "debug my last log is #{myfile}"
+    file=File.open(myfile) #read file
+    file_data=file.readlines.map(&:chomp) #attemping to get file data without new lines
+    file.close #closing file
+    filesize=0  #get size of the file
+    filesize=file_data.size
+    puts "'debug gamelog file has #{filesize}' lines"
+    #only run if file size is greater than 5
+    if filesize < 5
+     puts "exiting. Listener is active but file is too small. Try exiting the station."
+     exit
+    end
+  else 
+    puts "missing file #{myfile} exiting"
+    exit
   end
-  #######
-  ##This part is buggy not working as expected.
-  ##issues The array to string conversion is creating issues here.
-  #######
-  #get last 3 lines of the log
-  #PROBLEM
-  ###We 1..2 on warp_to_top.
-  ##jump count is 1. We are in warp...
-  ##waiting for completion
-  ##'debug filesize has 1926' lines
-  ##[ 2021.12.09 04:59:11 ] (notify) You cannot do that while warping.
-  ##[ 2021.12.09 04:59:49 ] (None) Jumping from Sarum Prime to Amarr
-  ##[ 2021.12.09 05:08:17 ] (notify) You cannot do that while warping.
-  ##["[ 2021.12.09 04:59:11 ] (notify) You cannot do that while warping.\r\n", "[ 2021.12.09 04:59:49 ] (None) Jumping from Sarum Prime to Amarr\r\n", "[ 2021.12.09 05:08:17 ] (notify) You cannot do that while warping.\r\n"]
-  last_3=IO.readlines(myfile)[-3..-1]
-  last_3.each do |line|
-    if /^\[/.match(line)#sometimes the lines don't have the time ignore them
-      if ! /notify/.match(line)# ignore lines with notify
-        result=is_log_entry_current(line.chomp) #current log entry only
-        if result ==1
-         array = line.split("(None) ")[1]#remove first part of line so just get the jumping info
-         return array.to_s.chomp #convert array to string
-        end
+  
+ #  puts "is file_data an array ?"
+ #  p file_data.instance_of? Array
+ 
+  last_3=file_data[-3..-1]  #get last 3 lines of the file_data
+ #  puts "is last_3 an array ?"
+ #  p last_3.instance_of? Array
+ 
+  last_3.each do |line|  #look at it
+    if /^\[/.match(line) #sometimes the lines don't have the time ignore them
+      result=is_log_entry_current(line.chomp) #current log entry only
+      if result ==1
+         string = line.split("(None) ")[1]#remove first part of line so just get the jumping info
+         # puts "is string an array ?"
+         # p string.instance_of? Array
+         return string
       end
     end
    end
+   return "" #return an empty string to prevent an object pointer from getting returned and messsing up things
  end
 
 ###################################################
