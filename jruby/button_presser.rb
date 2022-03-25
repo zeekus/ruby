7a3251b2a459c90969c0ab14dbd9a677cb89d822
Theodore Knab <ipmasqman@gmail.com>
2022-03-25 09:10:23 -0400

    added button presser

---
+++ jruby/button_presser.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/jruby
+#filename: button_presser.rb
+#description: sometimes you need a robot to press buttons for you. 
+
+require 'java'
+
+java_import 'java.awt.Robot'            #robot class
+
+
+def press_enter(robot,name)
+    @code = java.awt.event.KeyEvent.const_get("VK_#{name}")
+    robot.key_press(@code)
+    robot.key_release(@code)
+end
+
+robot = Robot.new
+counter=0
+BUTTON="ENTER"
+while true
+  puts "pressing #{BUTTON} #{counter}"
+  press_enter(robot,BUTTON)
+  sleep 5
+  counter +=1
+end
