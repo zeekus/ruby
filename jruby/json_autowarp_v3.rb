87190dee9bd5409acc48f4bbf35050f595e13dc1
zeekus <ipmasqman@gmail.com>
2022-04-21 14:25:43 -0400

    reverted to old logparse

--- jruby/json_autowarp_v3.rb
+++ jruby/json_autowarp_v3.rb
@@ -31,9 +31,9 @@ java_import 'java.awt.Toolkit'          #gets screens size
 
 start_time=Time.now.to_i #get time in secs
 
-class Logparse
+lass LogParser
      
-  def self.is_log_string_current(debug=1,loginfo,sec_threshold)
+  def is_log_string_current(debug,loginfo,sec_threshold)
     puts "is_log_string_current" + loginfo if debug ==1
     #string stripping log date time parser
     logtime=loginfo.gsub(/\(.*/,"").chomp                   #remove everything after the "(" character in the line
@@ -54,7 +54,7 @@ class Logparse
     end
   end
   
-  def self.log_reader(debug=1,target_phrase,log_size,sec_threshold)
+  def log_reader(debug=1,target_phrase,log_size,sec_threshold)
     if debug ==1
      puts "debug is #{debug}"
      puts "target_phrase is #{target_phrase}"
@@ -72,7 +72,6 @@ class Logparse
     potential_log_locations=["#{my_homedir}/EVE/logs/Gamelogs/*.txt", 
                              "#{my_homedir}/Games/eve-online/drive_c/users/#{user}/Documents/EVE/logs/Gamelog/*.txt", 
                              "/home/#{user}/Documents/EVE/logs/Gamelogs/*.txt"  ]
-
   
     myfile=""
     potential_log_locations.each do |logfile_loc_glob|
@@ -82,8 +81,7 @@ class Logparse
       #initialize variables
       capture_string=""
       myfile=Dir.glob(logfile_loc_glob).max_by { |file_name| File.ctime(file_name) } 
-      if myfile != nil and File.exists?(myfile) #we found a file. 
-        puts "we found file #{myfile}"
+      if File.exists?(myfile) #we found a file. 
         break #leaving loop
       end
     end
@@ -120,21 +118,127 @@ class Logparse
         puts "debug log_reader result is #{result}" if debug==1
         if line =~ /#{target_phrase}/i  and result==1
            if target_phrase =~ /Jumping/i or target_phrase =~ /Undocking/i 
-            capture_string = line.split(") ")[1]#remove first part of line so just get the jumping info
-           #capture important main notify messages
-           elsif target_phrase =~ /docking/i or target_phrase =~ /while warping/i or target_phrase =~ /please wait/i or target_phrase =~ /cloaking.*fails/i
-            capture_string = line.split(") ")[1]#remove first part of line so just get the docking or warping line  
+            capture_string = line.split("(None) ")[1]#remove first part of line so just get the jumping info
+           elsif target_phrase =~ /docking/i or target_phrase =~ /warping/i or target_phrase =~ /please wait.../i
+            capture_string = line.split("(notify) ")[1]#remove first part of line so just get the docking or warping line  
            elsif target_phrase =~ /combat/i
-            capture_string = line.split(") ")[1]  
+            capture_string = line.split("(combat) ")[1]
            else
             puts "" #do nothing
            end
         end
       end
     }
-    return capture_string.to_s #convert to string just in case
+    return capture_string #convert to string just in case
   end #function
 end #class
+
+# #NOT WORKING NEW
+
+# class Logparse
+     
+#   def self.is_log_string_current(debug=1,loginfo,sec_threshold)
+#     puts "is_log_string_current" + loginfo if debug ==1
+#     #string stripping log date time parser
+#     logtime=loginfo.gsub(/\(.*/,"").chomp                   #remove everything after the "(" character in the line
+#     logtime=loginfo.gsub(/(\[|\])/,"").chomp.strip          #remove brackets around the string and extra spaces aound the string
+#     #converting log time to usable format. Time source from log looks like [ 2021.12.02 19:21:20 UTC ].
+#     #Log example: [ 2021.12.01 21:50:52 ] (question) Are you sure you want to quit the game?
+#     logtime=DateTime.strptime(logtime, '%Y.%m.%d %H:%M:%S') #convert logtime sting to usable variable
+#     logtime_secs=logtime.strftime("%s")                     #convert time to seconds
+#     puts "debug log time- logtime_secs #{logtime_secs}" if debug==1
+#     current_secs=Time.now.utc.strftime("%s")                #our logs get current time in UTC seconds
+#     puts "debug - current_secs #{current_secs}" if debug==1
+#     diff=current_secs.to_i-logtime_secs.to_i                #calculate time diff in seconds from logs time to current time
+#     puts "debug: diff is #{diff}" if debug==1
+#     if diff < sec_threshold #theshold for number secs
+#       return 1 
+#     else
+#       return 0
+#     end
+#   end
+  
+#   def self.log_reader(debug=1,target_phrase,log_size,sec_threshold)
+#     if debug ==1
+#      puts "debug is #{debug}"
+#      puts "target_phrase is #{target_phrase}"
+#      puts "log_size is #{log_size}"
+#      puts "sec_threshold is #{sec_threshold}"
+#     end
+#     #####################
+#     #find latest log file
+#     #####################
+#     ##bash equivalent
+#     ##file=system("find /home/$USER/Documents/EVE/logs/Gamelogs -cmin -1 -exec ls -lah {} ';'")
+#     my_homedir=Dir.home #get homdir
+#     user=my_homedir.gsub(/.*\//,'') #get user
+    
+#     potential_log_locations=["#{my_homedir}/EVE/logs/Gamelogs/*.txt", 
+#                              "#{my_homedir}/Games/eve-online/drive_c/users/#{user}/Documents/EVE/logs/Gamelog/*.txt", 
+#                              "/home/#{user}/Documents/EVE/logs/Gamelogs/*.txt"  ]
+
+  
+#     myfile=""
+#     potential_log_locations.each do |logfile_loc_glob|
+#       puts "looking in #{logfile_loc_glob} for log file"
+#       limit=("-" + log_size.to_s).to_i #convert log size to negative number then back to integer
+#       last_log_entries=[] #empty array holding last log entries
+#       #initialize variables
+#       capture_string=""
+#       myfile=Dir.glob(logfile_loc_glob).max_by { |file_name| File.ctime(file_name) } 
+#       if myfile != nil and File.exists?(myfile) #we found a file. 
+#         puts "we found file #{myfile}"
+#         break #leaving loop
+#       end
+#     end
+ 
+#     if File.exists?(myfile)
+#       puts "*** log_reader my last log is #{myfile}" if debug==1
+#       file=File.open(myfile) #read file
+#       logfile_data=file.readlines.map(&:chomp) #attemping to get file data without new lines
+#       file.close #closing file
+#       puts "*** log_reader gamelog file has '#{logfile_data.size}' lines" if debug==1
+#       #only run if file size is greater than 5
+#       if logfile_data.size  < log_size
+#        puts "log_reader exiting. Listener is active but file is too small. Try exiting the station."
+#        exit
+#       else
+#         puts "*** log limit is [#{limit}..-1]" if debug==1 
+#       end
+#     else 
+#       if myfile !=null
+#         puts "log_reader missing file #{myfile} exiting"
+#       else 
+#         puts "log_reader file missing" 
+#       end
+#     end
+#     count=0
+#     capture_string="" #default is blank
+#     logfile_data.each  { |line|
+#       if /^\[/.match(line) #sometimes the lines don't have the time ignore them
+#         if debug ==1 
+#           puts "#{count}:#{line}"  #look at it
+#           count = count + 1
+#         end
+#         result=is_log_string_current(debug,line.chomp,sec_threshold) #current log entry only
+#         puts "debug log_reader result is #{result}" if debug==1
+#         if line =~ /#{target_phrase}/i  and result==1
+#            if target_phrase =~ /Jumping/i or target_phrase =~ /Undocking/i 
+#             capture_string = line.split(") ")[1]#remove first part of line so just get the jumping info
+#            #capture important main notify messages
+#            elsif target_phrase =~ /docking/i or target_phrase =~ /while warping/i or target_phrase =~ /please wait/i or target_phrase =~ /cloaking.*fails/i
+#             capture_string = line.split(") ")[1]#remove first part of line so just get the docking or warping line  
+#            elsif target_phrase =~ /combat/i
+#             capture_string = line.split(") ")[1]  
+#            else
+#             puts "" #do nothing
+#            end
+#         end
+#       end
+#     }
+#     return capture_string.to_s #convert to string just in case
+#   end #function
+# end #class
 
 class Viewer
 
