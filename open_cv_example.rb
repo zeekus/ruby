51b7d79e91d96c526549b6288e8a6d209f3cb1d0
Teddy Knab <31280061+zeekus@users.noreply.github.com>
2022-12-30 08:51:16 -0500

    Create open_cv_example.rb
    open_cv example for finding a pointer on a screen

---
+++ open_cv_example.rb
@@ -0,0 +1,15 @@
+#!/usb/bin/ruby
+#filename: open_cv_example.rb
+#description: find a pointer on a screen wth open ai.
+#requirements: pointer.png image 
+
+#capture the screen 
+capture = CvCapture.open
+frame = capture.query
+
+#identify the target image
+pointer = CvMat.load('pointer.png')
+
+#find the location of the pointer on the screen
+result = cvMatchTemplate(frame, pointer, :CV_TM_CCOEFF_NORMED)
+min_val, max_val, min_loc, max_loc = result.minMaxLoc
